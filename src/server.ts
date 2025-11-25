1df634656d10cecf5f77ea9bf83046ce013cf07a
Afnan Sayed <afnansayed1973@gmail.com>
2025-11-25 20:50:47 +0600

    create custom route

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,51 @@
 import http, { IncomingMessage, Server, ServerResponse } from 'http';
 import config from './confiq';
+import addRoute, { routes } from './helpers/RoutesHandler';
+
+addRoute('GET', '/', (req, res) => {
+  res.writeHead(200, { 'content-type': 'application/json' });
+  res.end(
+    JSON.stringify({
+      message: 'Hello from node js with typescript live server...',
+      path: req.url,
+    })
+  );
+});
 
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
-    console.log('Server is running ......');
-    if (req.url === '/' && req.method === 'GET') {
-      res.writeHead(200, { 'content-type': 'application/json' });
+    const method = req.method?.toLocaleUpperCase() || '';
+    const path = req.url || '';
+
+    const methodsMap = routes.get(method);
+    const handler = methodsMap?.get(path);
+
+    if (handler) {
+      handler(req, res);
+    } else {
+      res.writeHead(404, { 'content-type': 'application/json' });
       res.end(
         JSON.stringify({
-          message: 'Hello from node js with typescript...',
-          path: req.url,
+          success: false,
+          message: 'path not found',
+          path: path,
         })
       );
     }
+    console.log('Server is running ......');
   }
 );
 
 server.listen(config.port, () => {
   console.log(`Server is running on port ${config.port}`);
 });
+
+// if (req.url === '/' && req.method === 'GET') {
+//   res.writeHead(200, { 'content-type': 'application/json' });
+//   res.end(
+//     JSON.stringify({
+//       message: 'Hello from node js with typescript live server...',
+//       path: req.url,
+//     })
+//   );
+// }
