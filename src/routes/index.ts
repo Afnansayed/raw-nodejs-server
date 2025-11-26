ca8149021cf5ee63f5a9a11cfb22e9e880be1964
Afnan Sayed <afnansayed1973@gimal.com>
2025-11-26 12:39:51 +0600

    Feat: "post resques with clean functuinality"

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
+import parseBody from '../helpers/parseBody';
 import addRoute from '../helpers/RoutesHandler';
 import sendJson from '../helpers/sendJson';
 
@@ -12,5 +13,14 @@ addRoute('GET', '/api', (req, res) => {
   sendJson(res, 200, {
     message: 'Health status ok',
     path: req.url,
+  });
+});
+
+addRoute('POST', '/api/user', async (req, res) => {
+  const body = await parseBody(req);
+  sendJson(res, 200, {
+    success: true,
+    data: body,
+    path: req.url
   });
 });
